dd07fa3783cf76e026f8c6ef59f7a30e6003c5c1
Mark Goddard <mark@cofide.io>
2024-11-01 09:15:43 +0000

    Rework attestation policies to use a Kubernetes kind
    Updates the AttestationPolicy message in a backward incompatible way.
    
    - Removes the kind field in place of a oneof policy field
    - Kubernetes-specific proto message fields are in a APKubernetes
      message.
    - Namespace and pod label selectors use the Kubernetes label selector
      format, as defined by the LabelSelector message in [1].
    
    [1] https://github.com/kubernetes/apimachinery/blob/master/pkg/apis/meta/v1/generated.proto

--- gen/proto/trust_zone/v1/trust_zone.pb.go
+++ gen/proto/trust_zone/v1/trust_zone.pb.go
@@ -132,51 +132,6 @@ func (x *TrustZone) GetAttestationPolicies() []*v12.APBinding {
 	return nil
 }
 
-type TrustZoneList struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	TrustZones []*TrustZone `protobuf:"bytes,1,rep,name=trust_zones,json=trustZones,proto3" json:"trust_zones,omitempty"`
-}
-
-func (x *TrustZoneList) Reset() {
-	*x = TrustZoneList{}
-	mi := &file_proto_trust_zone_v1_trust_zone_proto_msgTypes[1]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
-}
-
-func (x *TrustZoneList) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*TrustZoneList) ProtoMessage() {}
-
-func (x *TrustZoneList) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_trust_zone_v1_trust_zone_proto_msgTypes[1]
-	if x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use TrustZoneList.ProtoReflect.Descriptor instead.
-func (*TrustZoneList) Descriptor() ([]byte, []int) {
-	return file_proto_trust_zone_v1_trust_zone_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *TrustZoneList) GetTrustZones() []*TrustZone {
-	if x != nil {
-		return x.TrustZones
-	}
-	return nil
-}
-
 var File_proto_trust_zone_v1_trust_zone_proto protoreflect.FileDescriptor
 
 var file_proto_trust_zone_v1_trust_zone_proto_rawDesc = []byte{
@@ -220,16 +175,11 @@ var file_proto_trust_zone_v1_trust_zone_proto_rawDesc = []byte{
 	0x32, 0x1e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e, 0x61, 0x70, 0x5f, 0x62, 0x69, 0x6e, 0x64,
 	0x69, 0x6e, 0x67, 0x2e, 0x76, 0x31, 0x2e, 0x41, 0x50, 0x42, 0x69, 0x6e, 0x64, 0x69, 0x6e, 0x67,
 	0x52, 0x13, 0x61, 0x74, 0x74, 0x65, 0x73, 0x74, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x50, 0x6f, 0x6c,
-	0x69, 0x63, 0x69, 0x65, 0x73, 0x22, 0x50, 0x0a, 0x0d, 0x54, 0x72, 0x75, 0x73, 0x74, 0x5a, 0x6f,
-	0x6e, 0x65, 0x4c, 0x69, 0x73, 0x74, 0x12, 0x3f, 0x0a, 0x0b, 0x74, 0x72, 0x75, 0x73, 0x74, 0x5f,
-	0x7a, 0x6f, 0x6e, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x1e, 0x2e, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x2e, 0x74, 0x72, 0x75, 0x73, 0x74, 0x5f, 0x7a, 0x6f, 0x6e, 0x65, 0x2e, 0x76,
-	0x31, 0x2e, 0x54, 0x72, 0x75, 0x73, 0x74, 0x5a, 0x6f, 0x6e, 0x65, 0x52, 0x0a, 0x74, 0x72, 0x75,
-	0x73, 0x74, 0x5a, 0x6f, 0x6e, 0x65, 0x73, 0x42, 0x3a, 0x5a, 0x38, 0x67, 0x69, 0x74, 0x68, 0x75,
-	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x6f, 0x66, 0x69, 0x64, 0x65, 0x2f, 0x63, 0x6f, 0x66,
-	0x69, 0x64, 0x65, 0x2d, 0x61, 0x70, 0x69, 0x2d, 0x73, 0x64, 0x6b, 0x2f, 0x67, 0x65, 0x6e, 0x2f,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x74, 0x72, 0x75, 0x73, 0x74, 0x5f, 0x7a, 0x6f, 0x6e, 0x65,
-	0x2f, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x69, 0x63, 0x69, 0x65, 0x73, 0x42, 0x3a, 0x5a, 0x38, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
+	0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x6f, 0x66, 0x69, 0x64, 0x65, 0x2f, 0x63, 0x6f, 0x66, 0x69, 0x64,
+	0x65, 0x2d, 0x61, 0x70, 0x69, 0x2d, 0x73, 0x64, 0x6b, 0x2f, 0x67, 0x65, 0x6e, 0x2f, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x2f, 0x74, 0x72, 0x75, 0x73, 0x74, 0x5f, 0x7a, 0x6f, 0x6e, 0x65, 0x2f, 0x76,
+	0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -244,24 +194,22 @@ func file_proto_trust_zone_v1_trust_zone_proto_rawDescGZIP() []byte {
 	return file_proto_trust_zone_v1_trust_zone_proto_rawDescData
 }
 
-var file_proto_trust_zone_v1_trust_zone_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_proto_trust_zone_v1_trust_zone_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_proto_trust_zone_v1_trust_zone_proto_goTypes = []any{
 	(*TrustZone)(nil),        // 0: proto.trust_zone.v1.TrustZone
-	(*TrustZoneList)(nil),    // 1: proto.trust_zone.v1.TrustZoneList
-	(*v1.TrustProvider)(nil), // 2: proto.trust_provider.v1.TrustProvider
-	(*v11.Federation)(nil),   // 3: proto.federation.v1.Federation
-	(*v12.APBinding)(nil),    // 4: proto.ap_binding.v1.APBinding
+	(*v1.TrustProvider)(nil), // 1: proto.trust_provider.v1.TrustProvider
+	(*v11.Federation)(nil),   // 2: proto.federation.v1.Federation
+	(*v12.APBinding)(nil),    // 3: proto.ap_binding.v1.APBinding
 }
 var file_proto_trust_zone_v1_trust_zone_proto_depIdxs = []int32{
-	2, // 0: proto.trust_zone.v1.TrustZone.trust_provider:type_name -> proto.trust_provider.v1.TrustProvider
-	3, // 1: proto.trust_zone.v1.TrustZone.federations:type_name -> proto.federation.v1.Federation
-	4, // 2: proto.trust_zone.v1.TrustZone.attestation_policies:type_name -> proto.ap_binding.v1.APBinding
-	0, // 3: proto.trust_zone.v1.TrustZoneList.trust_zones:type_name -> proto.trust_zone.v1.TrustZone
-	4, // [4:4] is the sub-list for method output_type
-	4, // [4:4] is the sub-list for method input_type
-	4, // [4:4] is the sub-list for extension type_name
-	4, // [4:4] is the sub-list for extension extendee
-	0, // [0:4] is the sub-list for field type_name
+	1, // 0: proto.trust_zone.v1.TrustZone.trust_provider:type_name -> proto.trust_provider.v1.TrustProvider
+	2, // 1: proto.trust_zone.v1.TrustZone.federations:type_name -> proto.federation.v1.Federation
+	3, // 2: proto.trust_zone.v1.TrustZone.attestation_policies:type_name -> proto.ap_binding.v1.APBinding
+	3, // [3:3] is the sub-list for method output_type
+	3, // [3:3] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
 }
 
 func init() { file_proto_trust_zone_v1_trust_zone_proto_init() }
@@ -275,7 +223,7 @@ func file_proto_trust_zone_v1_trust_zone_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_trust_zone_v1_trust_zone_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
